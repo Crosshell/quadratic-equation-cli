fbcf2ef1e5d8164c38a70f791546510b4c21b163
Crosshell <confighell@gmail.com>
2025-02-28 00:58:59 +0200

    Improve value processing

--- src/non-interactive.ts
+++ src/non-interactive.ts
@@ -2,9 +2,16 @@ import fs from 'node:fs/promises';
 
 export async function startNonInteractive(filePath: string): Promise<void> {
   const content: string = (await fs.readFile(filePath, 'utf-8')).trim();
-  const coeffs: number[] = content.split(' ').map(parseFloat);
+  const nums: number[] = content.split(' ').map(parseFloat);
 
-  if (coeffs.length !== 3 || coeffs.some(isNaN)) {
+  if (nums.length !== 3 || nums.some(isNaN)) {
     throw new Error('Invalid file format');
   }
+
+  const [a, b, c] = nums;
+  const coeffs: Coefficients = { a, b, c };
+
+  console.log(
+    `Equation is: (${coeffs.a}) x^2 + (${coeffs.b}) x + (${coeffs.c}) = 0`,
+  );
 }
