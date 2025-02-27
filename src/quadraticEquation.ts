5efe977f8ed6c55bdd2fe9c3ed29be05596e87ee
Crosshell <confighell@gmail.com>
2025-02-27 17:41:04 +0200

    add QuadraticEquation class added a function to calculate the discriminant added a function to compute the equation's roots included validation to prevent a = 0

---
+++ src/quadraticEquation.ts
@@ -0,0 +1,43 @@
+class QuadraticEquation {
+  private readonly a: number;
+  private readonly b: number;
+  private readonly c: number;
+  private readonly discriminant: number;
+  private readonly roots: number[];
+
+  constructor(a: number, b: number, c: number) {
+    if (a === 0) {
+      throw new Error('Coefficient "a" must not be 0');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.discriminant = this.calculateDiscriminant();
+    this.roots = this.calculateRoots();
+  }
+
+  private calculateDiscriminant(): number {
+    return this.b ** 2 - 4 * this.a * this.c;
+  }
+
+  private calculateRoots(): number[] {
+    if (this.discriminant < 0) {
+      return [];
+    }
+
+    const sqrtD: number = Math.sqrt(this.discriminant);
+
+    if (this.discriminant > 0) {
+      return [
+        (-this.b + sqrtD) / (2 * this.a),
+        (-this.b - sqrtD) / (2 * this.a),
+      ];
+    }
+
+    return [-this.b / (2 * this.a)];
+  }
+
+  getRoots(): number[] {
+    return this.roots;
+  }
+}
